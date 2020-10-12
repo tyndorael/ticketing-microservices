be577c83849c5dc8962c72df4c38b646461609be
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2020-10-12 13:38:35 -0300

    refactor nats-test project

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from "node-nats-streaming";
+import nats from "node-nats-streaming";
 import { randomBytes } from "crypto";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
 console.clear();
 
@@ -14,22 +15,7 @@ stan.on('connect', () => {
     console.log('NATS connection closed!');
   });
 
-  const options = stan.subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('listener-service');
-
-  const subscription = stan.subscribe('ticket:created', 'listener-queue-group', options);
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Received event # ${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
