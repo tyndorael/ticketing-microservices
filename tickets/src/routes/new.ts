032f8d4f99e68b83487ac52569eeecd837cf7e3c
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2020-10-13 23:53:46 -0300

    add ticket events

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,12 +3,10 @@ import { body } from 'express-validator';
 import nats from 'node-nats-streaming';
 import { requireAuth, validateRequest } from '@tynsoft/common';
 import { Ticket } from '../models/ticket';
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
-
-/* const stan = nats.connect('ticketing', 'tickets', {
-  url: 'http://nats-srv:4222',
-}); */
 
 router.post(
   '/api/tickets',
@@ -30,15 +28,14 @@ router.post(
     });
     await ticket.save();
 
-    const event = {
-      type: 'ticket:created',
-      data: ticket,
-    };
-    /*stan.publish('ticket:created', JSON.stringify(event), () => {
-      console.log('Ticket creation event published');
-    });*/
+    await new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
 
-    res.status(201).send(ticket);
+    return res.status(201).send(ticket);
   }
 );
 
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
   NotAuthorizedError,
 } from '@tynsoft/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -37,7 +39,14 @@ router.put(
     });
     await ticket.save();
 
-    res.send(ticket);
+    await new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId
+    });
+
+    return res.send(ticket);
   }
 );
 
