3230e039676c2784e081eb041f020ec6ad01e099
Alejandro Oyarzún Sepúlveda <aosepulveda@outlook.com>
2020-10-25 14:26:46 -0300

    fix ticket update listener for orders ms

--- orders/src/events/listeners/ticket-updated-listener.ts
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -1,16 +1,16 @@
-import { Message } from "node-nats-streaming";
-import { Listener, Subjects, TicketUpdatedEvent } from "@tynsoft/common";
+import { Message } from 'node-nats-streaming';
+import { Listener, Subjects, TicketUpdatedEvent } from '@tynsoft/common';
 import { queueGroupName } from './queue-group-name';
-import { Ticket } from "../../models/ticket";
+import { Ticket } from '../../models/ticket';
 
 export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName = queueGroupName;
 
-  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const ticket = await Ticket.findOne(data.id);
+  async onMessage(data: TicketUpdatedEvent['data'], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
 
-    if(!ticket) {
+    if (!ticket) {
       throw new Error('Ticket not found');
     }
 
